36729c110313af283ca604d3b6026d0f45dfd6cf
Tina9 <xuhzhang@outlook.com>
2023-07-26 10:55:23 +0800

    Add explanation in config file

---
+++ config.py
@@ -0,0 +1,18 @@
+chatbotParm = {
+	'openai.api_key': "Your API key of GPT"
+}
+
+Qprompt = """
+
+The prompt for Q&A of the tool
+"""
+
+ASprompt = """
+
+The prompt to automatically explain the result for the potential prognosis biomarker
+"""
+
+AFprompt = """
+
+The prompt to automatically explain why candidate genes cannot be used as a prognosis biomarker
+"""
